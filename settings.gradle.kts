68fdf9d60705cda5c51ed68f1cced4b41b0ca4b9
m-gjurevski-nano-ic <155987292+m-gjurevski-nano-ic@users.noreply.github.com>
2024-03-07 12:59:45 -0500

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SPOTIFY_API_APP"
+include(":app")
