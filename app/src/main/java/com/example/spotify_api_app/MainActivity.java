cc723a9f394c2ba272f146dbc4443665e347e89a
Reedric <86981900+Reedric@users.noreply.github.com>
2024-03-26 13:48:10 -0400

    March 26 commit to see if Marko or Andrew can resolve their spotify login bug

--- app/src/main/java/com/example/spotify_api_app/MainActivity.java
+++ app/src/main/java/com/example/spotify_api_app/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.spotify.sdk.android.auth.AuthorizationClient;
 import com.spotify.sdk.android.auth.AuthorizationRequest;
 import com.spotify.sdk.android.auth.AuthorizationResponse;
