969a0d991cf95e278cee5da79bf19531699f20d7
Andrew Boyer <asboyer@gmail.com>
2024-04-14 16:07:47 -0400

    fixing gradle bug

--- app/src/main/java/com/example/spotify_api_app/MainProfileActivity.java
+++ app/src/main/java/com/example/spotify_api_app/MainProfileActivity.java
@@ -8,7 +8,6 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
